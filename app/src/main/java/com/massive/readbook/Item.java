36b796fa11630180a0636fad4c3625bad7c16f69
kerofawzy2055@gmail.com <kerofawzy2055@gmail.com>
2017-01-27 19:58:49 +0200

    final project

--- app/src/main/java/com/massive/readbook/Item.java
+++ app/src/main/java/com/massive/readbook/Item.java
@@ -1,107 +1,54 @@
 package com.massive.readbook;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * Created by Minafaw on 27/01/2017.
  */
 
-public class Item implements Serializable
-{
+public class Item implements Serializable, Parcelable {
     public String kind ;
     public String id ;
-    public String etag ;
-    public String selfLink ;
     public VolumeInfo volumeInfo ;
-    public SaleInfo saleInfo ;
-    public AccessInfo accessInfo ;
-    public SearchInfo searchInfo ;
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.kind);
+        dest.writeString(this.id);
+        dest.writeParcelable(this.volumeInfo, flags);
+    }
+
+    public Item() {
+    }
+
+    protected Item(Parcel in) {
+        this.kind = in.readString();
+        this.id = in.readString();
+        this.volumeInfo = in.readParcelable(VolumeInfo.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Item> CREATOR = new Parcelable.Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel source) {
+            return new Item(source);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
 }
 
-class IndustryIdentifier
-{
-    public String type ;
-    public String identifier ;
-}
 
- class ReadingModes
-{
-    public boolean text ;
-    public boolean image ;
-}
-
-class ListPrice
-{
-    public double amount ;
-    public String currencyCode ;
-}
-
- class RetailPrice
-{
-    public double amount ;
-    public String currencyCode ;
-}
-
- class ListPrice2
-{
-    public double amountInMicros ;
-    public String currencyCode ;
-}
-
- class RetailPrice2
-{
-    public double amountInMicros ;
-    public String currencyCode ;
-}
-
- class Offer
-{
-    public int finskyOfferType ;
-    public ListPrice2 listPrice ;
-    public RetailPrice2 retailPrice ;
-}
-
- class SaleInfo
-{
-    public String country ;
-    public String saleability ;
-    public boolean isEbook ;
-    public ListPrice listPrice ;
-    public RetailPrice retailPrice ;
-    public String buyLink ;
-    public List<Offer> offers ;
-}
-
- class Epub
-{
-    public boolean isAvailable ;
-    public String acsTokenLink ;
-}
-
- class Pdf
-{
-    public boolean isAvailable ;
-    public String acsTokenLink ;
-}
-
- class AccessInfo
-{
-    public String country ;
-    public String viewability ;
-    public boolean embeddable ;
-    public boolean publicDomain ;
-    public String textToSpeechPermission ;
-    public Epub epub ;
-    public Pdf pdf ;
-    public String webReaderLink ;
-    public String accessViewStatus ;
-    public boolean quoteSharingAllowed ;
-}
-
- class SearchInfo
-{
-    public String textSnippet ;
-}
 
 
--- app/src/main/java/com/massive/readbook/VolumeInfo.java
+++ app/src/main/java/com/massive/readbook/VolumeInfo.java
@@ -1,18 +1,21 @@
 package com.massive.readbook;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 import java.util.List;
 
-public class VolumeInfo implements Serializable
-{
+public class VolumeInfo implements Serializable, Parcelable {
+  private static final long serialVersionUID = 100L;
+
   public String title ;
   public String subtitle ;
   public List<String> authors ;
   public String publisher ;
   public String publishedDate ;
   public String description ;
-  public List<IndustryIdentifier> industryIdentifiers ;
-  public ReadingModes readingModes ;
+
   public int pageCount ;
   public String printType ;
   public List<String> categories ;
@@ -26,4 +29,71 @@ public class VolumeInfo implements Serializable
   public String previewLink ;
   public String infoLink ;
   public String canonicalVolumeLink ;
+
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(this.title);
+    dest.writeString(this.subtitle);
+    dest.writeStringList(this.authors);
+    dest.writeString(this.publisher);
+    dest.writeString(this.publishedDate);
+    dest.writeString(this.description);
+
+    dest.writeInt(this.pageCount);
+    dest.writeString(this.printType);
+    dest.writeStringList(this.categories);
+    dest.writeDouble(this.averageRating);
+    dest.writeInt(this.ratingsCount);
+    dest.writeString(this.maturityRating);
+    dest.writeByte(this.allowAnonLogging ? (byte) 1 : (byte) 0);
+    dest.writeString(this.contentVersion);
+    dest.writeSerializable(this.imageLinks);
+    dest.writeString(this.language);
+    dest.writeString(this.previewLink);
+    dest.writeString(this.infoLink);
+    dest.writeString(this.canonicalVolumeLink);
+  }
+
+  public VolumeInfo() {
+  }
+
+  protected VolumeInfo(Parcel in) {
+    this.title = in.readString();
+    this.subtitle = in.readString();
+    this.authors = in.createStringArrayList();
+    this.publisher = in.readString();
+    this.publishedDate = in.readString();
+    this.description = in.readString();
+    this.pageCount = in.readInt();
+    this.printType = in.readString();
+    this.categories = in.createStringArrayList();
+    this.averageRating = in.readDouble();
+    this.ratingsCount = in.readInt();
+    this.maturityRating = in.readString();
+    this.allowAnonLogging = in.readByte() != 0;
+    this.contentVersion = in.readString();
+    this.imageLinks = (ImageLinks) in.readSerializable();
+    this.language = in.readString();
+    this.previewLink = in.readString();
+    this.infoLink = in.readString();
+    this.canonicalVolumeLink = in.readString();
+  }
+
+  public static final Parcelable.Creator<VolumeInfo> CREATOR = new Parcelable.Creator<VolumeInfo>() {
+    @Override
+    public VolumeInfo createFromParcel(Parcel source) {
+      return new VolumeInfo(source);
+    }
+
+    @Override
+    public VolumeInfo[] newArray(int size) {
+      return new VolumeInfo[size];
+    }
+  };
 }
